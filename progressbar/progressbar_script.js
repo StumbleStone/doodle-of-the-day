47f1e6cacdc4394c5548868efde2e549ffc46e4d
Wessels <wessels@journeyapps.com>
2022-03-09 22:13:26 +0200

    Added standard looking progressbar

--- progressbar/progressbar_script.js
+++ progressbar/progressbar_script.js
@@ -100,10 +100,87 @@ class progressbarGenerator {
             const lightStart = 100 - (20 * segPercentage).toFixed(2);
 
             segmentsArr[curSeg].style.background = `linear-gradient(hsl(100deg, 50%, ${lightStart}%) 20%, hsl(100deg, 50%, ${light}%), hsl(100deg, 50%, ${lightStop}%)) 20%`;
-
-
-            console.log(value, minSegValue, maxSegValue, curSeg, segPercentage);
+            // console.log(value, minSegValue, maxSegValue, curSeg, segPercentage);
         }
+    }
+
+    oldSchoolSandbox(params){
+        const newTile = AddTile(params.title ?? "ProgressBar");
+        const bBox = newTile.getBoundingClientRect();
+
+        const container = this.generateContainer();
+
+        const segments = 5;
+
+        const segmentWidth = bBox.width / segments;
+
+        const segArr = [];
+
+        for(let seg = 0; seg < segments; seg ++){
+            const segDiv = document.createElement("div");
+
+            segDiv.style.width = segmentWidth + "px";
+            segDiv.style.height = segmentWidth + "px";
+
+            segDiv.style.display = "inline-block";
+
+            // segDiv.style.border = "1px solid black";
+
+            container.appendChild(segDiv);
+            segArr.push(segDiv);
+        }
+
+        const drawBars = (segmentItem, perc) => {
+            const bars = Math.ceil(perc * segments);
+            if(segmentItem.children.length === bars){
+                return;
+            }
+
+            if(segmentItem.children.length > bars){
+                while(segmentItem.children.length > bars){
+                    const lastBarChild = [...segmentItem.children].pop();
+                    lastBarChild.remove();
+                }
+                return;
+            }
+
+            const generateBar = (pos) => {
+                const bar = document.createElement("div");
+                bar.style.width = "20%";
+                bar.style.display = "inline-block";
+                bar.style.height = "100%";
+                bar.style.background = `linear-gradient(hsl(200deg, 50%, ${60}%) 20%, hsl(200deg, 50%, ${40}%), hsl(200deg, 50%, ${30}%)) 20%`;
+                return bar;
+            }
+
+            while(segmentItem.children.length < bars){
+                segmentItem.appendChild(generateBar(segmentItem.children.length));
+            }
+        }
+
+        const updater = (value) => {
+            const curSeg = Math.floor(value / (100 / segments));
+
+            const minSegValue = Math.floor(curSeg * (100 / segments));
+            const maxSegValue = Math.floor((curSeg + 1) * (100 / segments));
+
+            const segPercentage = (value - minSegValue) / (maxSegValue - minSegValue);
+
+            // console.log(value, minSegValue, maxSegValue, curSeg, segPercentage);
+            drawBars(segArr[curSeg], segPercentage);
+        }
+
+        container.style.borderRadius = "10px";
+        container.style.overflow = "hidden";
+        container.style.backgroundColor = "#ffffff";
+        container.style.textAlign = "left";
+
+        newTile.appendChild(container);
+
+        newTile.id = "ProgressBar_" + (params.title ?? "Unknown");
+        this.renders.push(newTile);
+
+        return updater;
     }
 
     createProgressBar(params) {
@@ -129,14 +206,22 @@ class progressbarGenerator {
         this.updaters.push(updater);
     }
 
+    createOldSchoolProgressBar(date){
+        const updater = this.oldSchoolSandbox({
+            title: date,
+        })
+        this.updaters.push(updater);
+    }
+
     render() {
         dateChecker("2022-03-11", (date) => this.createSegmentProgressBar(date));
+        dateChecker("2022-03-12", (date) => this.createOldSchoolProgressBar(date));
     }
 
     close(){
         clearInterval(this.updaterInterval)
         for(let curRender of this.renders){
-            console.log("SpinnerGenerator", "Removing render Tile:", curRender.id);
+            console.log("ProgressBarGenerator", "Removing render Tile:", curRender.id);
             curRender.remove();
         }
     }
